58e93def525d87e2952437cef27b21f91484caaa
Brenda Lopez <bmlc_gpo316@outlook.com>
2019-10-08 22:06:32 -0500

    Práctica 9

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* 
+Autor: Brenda Montserrat López Castro
+Fecha: 08/Octubre/2019
+Grupo: 08
+Horario: Sábados 09-11 hrs
+Matrícula: 1744884
+*/
 
-int main(int argc, char *argv[]) {
+/****************Actividad 1*****************/
+int main(){
+	int ventasEmpleadoOficinaEdificio[6][10][2];
+	ventasEmpleadoOficinaEdificio[3][7][2] = 4234;
+	printf("El empleado numero 3 de la oficina numero 7 del edificio numero 2 ha vendido %d\n\n", ventasEmpleadoOficinaEdificio[3][7][2]);
+	fflush (stdin);
+	
+/***************Actividad 2*****************/
+	ventasEmpleadoOficinaEdificio[3][7][2] = ventasEmpleadoOficinaEdificio[3][7][2]+10;
+	printf("El empleado numero 3 de la oficina numero 7 del edificio numero 2 ha vendido %d", ventasEmpleadoOficinaEdificio[3][7][2]);
 	return 0;
 }
+
+/* Preguntas de actividad 1
+
+1. ¿Cuántos elementos componen el array multidimensional?
+    R = 120 elementos
+	
+2. ¿Qué ocurre si intentamos mostrar por pantalla un valor con un índice que está definido para ese array multidimensional?
+    R = el programa almacena y arroja valores basura o valores aleatorios si no se definen con un valor específico
+
+*/
+
+
