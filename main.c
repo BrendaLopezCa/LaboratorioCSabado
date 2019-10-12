a354bfd8d3cf53707ffb70a1be65106d897df8d7
Brenda López <bmlc_gpo316@outlook.com>
2019-10-12 10:50:49 -0500

    Pŕactica 10

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* 
+Autor: Brenda Montserrat López Castro
+Fecha: 12/octubre/2019
+Grupo: 08
+Horario: sábados 9-11 a.m.
+Matrícula: 1744884
+*/
 
-int main(int argc, char *argv[]) {
+/*         Actividad 1          */
+int main() {
+	char cadena1[20] = {"A-Z"}, cadena2[20] = {"Z-A"}, array[20] = {"A-Z"};/*declaracion de las cadenas y arreglos de cadenas a establecer*/
+	int i, j;
+	printf("Ingrese una frase no mayor a 20 caracteres\n"); /*pedir caracteres al usuario*/
+	scanf("%s %s",  &cadena1[20], &cadena2[20]); /*obtener caracteres y almacenarlos en los arrays*/
+	if (cadena1[20] == cadena2[20]) /*ciclo "if" para comparar los caracteres obtenidos y determinar si la cadena es un palíndromo*/
+		printf("La cadena de texto es un palíndromo\n\n\n"); /*verdadero*/
+	else
+		printf("La cadena de texto no es un palíndromo\n\n\n"); /*falso*/
+
+
+/*          Actividad 2          */
+
+	printf("Ingrese 20 caracateres");
+	scanf("%s", &array[20]);
+	{	
+		for ()
+	}
 	return 0;
 }
