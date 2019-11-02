f409fbdae57a6ee01c1374b44e9577e2fb85c261
Brenda López <bmlc_gpo316@outlook.com>
2019-11-02 10:09:36 -0600

    Practica13

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+Autor: Brenda Montserrat López Castro
+Fecha: 02/Nov/2019
+Grupo: 08
+Horario: sábados 9-11 a.m.
+Matrícula: 1744884 
+*/
 
-int main(int argc, char *argv[]) {
-	return 0;
+int main() 
+{
+   int a, b, c; /*Declaraci{on de variables y punteros*/
+   int *p1, *p2, *p3;
+   printf("Ingrese el valor de a:\n");
+   scanf("%d", &a);
+   printf("Ingrese el valor de b:\n");
+   scanf("%d", &b);
+   c=a+b;
+   printf("a+b=%d\n", c); /*asignamos las direcciones a los punteros correspondientes*/
+   p1=&a;
+   p2=&b;
+   p3=&c;
+   printf("*p1 + *p2=%d\n", *p1+*p2);
+   printf(" Dirección de a es %p\n Dirección de b es %p\n Y la de c es %p\n\n", p1, p2, p3);
+   getch();
+   return 0;
 }
